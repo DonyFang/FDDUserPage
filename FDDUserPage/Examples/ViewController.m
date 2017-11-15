5be1318816f5e8ca9d7913334005cd0fc2fae774
方冬冬 <Fang_DongDong@want-want.com>
2017-11-15 10:27:25 +0800

    commit

--- FDDUserPage/Examples/ViewController.m
+++ FDDUserPage/Examples/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "FDDUserPage.h"
 
 @interface ViewController ()
 
@@ -17,12 +18,53 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+//
+//    if (![[NSUserDefaults standardUserDefaults] boolForKey:BOOLFORKEY]) {
+//        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:BOOLFORKEY];
+//        // 静态引导页
+        [self setStaticGuidePage];
+//
+//        // 动态引导页
+//         [self setDynamicGuidePage];
+//
+//        // 视频引导页
+//        // [self setVideoGuidePage];
+//    }
+//
+    // 设置该控制器背景图片
+    UIImageView *bg_imageView = [[UIImageView alloc] initWithFrame:self.view.frame];
+    [bg_imageView setImage:[UIImage imageNamed:@"view_bg_image"]];
+    [self.view addSubview:bg_imageView];
+    [self setTitle:@"Come On 2017"];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+#pragma mark - 设置APP静态图片引导页
+- (void)setStaticGuidePage {
+    NSArray *imageNameArray = @[@"guideImage1.jpg",@"guideImage2.jpg",@"guideImage3.jpg",@"guideImage4.jpg",@"guideImage5.jpg"];
+    FDDUserPage *guidePage = [[FDDUserPage alloc] initWithFrame:self.view.frame imageNameArray:imageNameArray buttonIsHidden:NO userPageType:FDDUserPageNormal];
+    guidePage.slideInto = YES;
+//    guidePage.pageIndicatorTintColor = [UIColor grayColor];
+    [self.navigationController.view addSubview:guidePage];
+}
+
+#pragma mark - 设置APP动态图片引导页
+- (void)setDynamicGuidePage {
+    NSArray *imageNameArray = @[@"guideImage6.gif",@"guideImage7.gif",@"guideImage8.gif"];
+    FDDUserPage *guidePage = [[FDDUserPage alloc] initWithFrame:self.view.frame imageNameArray:imageNameArray buttonIsHidden:YES userPageType:FDDUserPageGif];
+    guidePage.slideInto = YES;
+    [self.navigationController.view addSubview:guidePage];
+}
+
+#pragma mark - 设置APP视频引导页
+- (void)setVideoGuidePage {
+    NSURL *videoURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"guideMovie1" ofType:@"mov"]];
+    FDDUserPage *guidePage = [[FDDUserPage alloc] initWithFrame:self.view.bounds videoURL:videoURL];
+    [self.navigationController.view addSubview:guidePage];
 }
 
 
--- FDDUserPage/FDDUserPage/FDDUserPage.h
+++ FDDUserPage/FDDUserPage/FDDUserPage.h
@@ -7,7 +7,42 @@
 //
 
 #import <UIKit/UIKit.h>
-
+typedef enum : NSUInteger {
+    FDDUserPageNormal,//默认启动图
+    FDDUserPageGif,//gif启动图
+} FDDUserPageType;
 @interface FDDUserPage : UIView
+//当前选中的颜色
+@property(nonatomic,strong)UIColor *currentPageControlColor;
+
+/**
+ 没有选中的颜色
+ */
+@property(nonatomic,strong)UIColor *pageIndicatorTintColor;
+/**
+ *  是否支持滑动进入APP(默认为NO-不支持滑动进入APP | 只有在buttonIsHidden为YES-隐藏状态下可用; buttonIsHidden为NO-显示状态下直接点击按钮进入)
+ *  新增视频引导页同样不支持滑动进入APP
+ */
+@property (nonatomic, assign) BOOL slideInto;
+/**
+ *  FDDUserPage(图片引导页 | 可自动识别动态图片和静态图片)
+ *
+ *  @param frame      位置大小
+ *  @param imageNameArray 引导页图片数组(NSString)
+ *  @param isHidden   开始体验按钮是否隐藏(YES:隐藏-引导页完成自动进入APP首页; NO:不隐藏-引导页完成点击开始体验按钮进入APP主页)
+ *
+ *  @return FDDUserPage对象
+ */
+- (instancetype)initWithFrame:(CGRect)frame imageNameArray:(NSArray<NSString *> *)imageNameArray buttonIsHidden:(BOOL)isHidden userPageType:(FDDUserPageType)userPageType;
+/**
+ *  FDDUserPage(视频引导页)
+ *
+ *  @param frame    位置大小
+ *  @param videoURL 引导页视频地址
+ *
+ *  @return FDDUserPage对象
+ */
+- (instancetype)initWithFrame:(CGRect)frame videoURL:(NSURL *)videoURL;
+
 
 @end
